5ba6f9c843e778c5015fec5aacf2953c041d1d18
saikrupa82 <46783175+saikrupa82@users.noreply.github.com>
2022-10-01 10:26:07 +0530

    Pythagoras Theorem
    Pythagoras Theorem

---
+++ pythagoras.py
@@ -0,0 +1,15 @@
+def pythagoras(opposite, adjacent, hypotenuse):
+    """
+    Returns length of a third side of a right angled triangle.
+    Passing "?" will indicate the unknown side.
+    """
+    try:
+        if opposite == str("?"):
+            return ("Opposite = " + str(((hypotenuse**2) - (adjacent**2))**0.5))
+        if adjacent == str("?"):
+            return ("Adjacent = " + str(((hypotenuse**2) - (opposite**2))**0.5))
+        if hypotenuse == str("?"):
+            return ("Hypotenuse = " + str(((opposite**2) + (adjacent**2))**0.5))
+        return "You already know the answer!"
+    except:
+        raise ValueError("invalid argument(s) were given.")
